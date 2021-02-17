3789b012bdc8f15c61e643e83cc0773ab096c78c
Dani <olivaresroza@gmail.com>
2021-02-17 19:38:40 +0100

    get-historical-played-games

--- tests/Api/UserActivityTest.php
+++ tests/Api/UserActivityTest.php
@@ -20,12 +20,27 @@ class UserActivityTest extends TestCase {
         $this->assertIsArray($response->get('historicalScoreAndSkills'));
     }
 
-    /*
     public function testGetPlayedGames()
     {
         $healthCheckInstance    = new UserActivity(getenv('TEST_CLIENT_ID'), getenv('TEST_CLIENT_SECRET'), true);
         $response               = $healthCheckInstance->getPlayedGames(getenv('TEST_USER_ID'));
-    }*/
+        $this->assertEquals(false, $response->hasError());
+        $this->assertIsArray($response->getData());
+        $this->assertArrayHasKey('success', $response->getData());
+        $this->assertEquals(1, $response->get('success'));
+        $this->assertArrayHasKey('historicalPlayedGames', $response->getData());
+        $this->assertIsArray($response->get('historicalPlayedGames'));
+        $lastPlayedGame = $response->get('historicalPlayedGames')[0];
+        $this->assertIsArray($lastPlayedGame);
+        $this->assertArrayHasKey('key', $lastPlayedGame);
+        $this->assertArrayHasKey('level', $lastPlayedGame);
+        $this->assertArrayHasKey('sublevel', $lastPlayedGame);
+        $this->assertArrayHasKey('score', $lastPlayedGame);
+        $this->assertArrayHasKey('timePlayed', $lastPlayedGame);
+        $this->assertArrayHasKey('time', $lastPlayedGame);
+        $this->assertRegExp('/^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]$/', $lastPlayedGame['time']);
+        $this->assertArrayHasKey('outReasonKey', $lastPlayedGame);
+    }
 
     public function testGetHistoricalScoreAndSkillsFail()
     {
