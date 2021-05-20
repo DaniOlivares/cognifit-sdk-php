7269e2ddebebe2dcfef71e576d86bf84a96e1976
Dani <olivaresroza@gmail.com>
2021-05-21 04:26:50 +1200

    Add locale to Issue Token

--- src/Api/UserAccessToken.php
+++ src/Api/UserAccessToken.php
@@ -8,8 +8,12 @@ class UserAccessToken extends Request {
 
 	const RESOURCE_PATH = '/issue-access-token';
 
-	public function issue(string $userId){
-		return $this->doRequest(self::RESOURCE_PATH, ['user_token' => $userId], 'POST');
+	public function issue(string $userId, string $locale = ''){
+	    $params = ['user_token' => $userId];
+	    if($locale){
+	        $params['locale'] = $locale;
+        }
+		return $this->doRequest(self::RESOURCE_PATH, $params, 'POST');
 	}
 	
 }
